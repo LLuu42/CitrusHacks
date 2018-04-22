d46de500206b51077ad1a951ae367097daa5b132
JTan04 <jtan26@calpoly.edu>
2018-04-21 20:04:59 -0700

    Update views.py

--- journal/views.py
+++ journal/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def getAuthToken():
    params = {"grant_type" : "client_credentials"}
-   headers = {"Authorization": "Bearer BQCTKaxHhr7CgpKLksP_Wf8oUGZ6Jkib2_aMOQv1tk__xzN-vtLnzuBRSfhQN2rKw7_ICO3XVy3HXitP4mv1ij3we0sMpTTX9_Y4igPCOhFWA-Oh2aJUB5Yq6ENLkhnL-3fb9IOUIQSp_QXIG9XJiO7PRzEBo81LkOmQs-3M9_aHmRPxXpKHUs3lC-aPAnkVtp-QKAWKnMEAv-I_uhC8mgKxlGT_w4MuplMNB5QXlPelc25upkVlACyOnZKLtBQoHk0krpmhqolY"}
+   headers = {"Authorization": "Basic 377ae4ed0bf04d73897ed02e8c8ecf92bacb8d11d69f40fc864ce62882822ea0="}
    res = unirest.post("https://accounts.spotify.com/api/token", params=params, headers=headers)
    return res.body['access_token']
 
